284f7fba5aa1cc7b0adb4c42c8f5ca6ba07d2f3e
szmo <szymonduda10@gmail.com>
2017-11-16 23:06:14 +0100

    fix registration problem

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111205747) do
+ActiveRecord::Schema.define(version: 20171116215911) do
 
   create_table "users", force: :cascade do |t|
-    t.string "login", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,7 +26,6 @@ ActiveRecord::Schema.define(version: 20171111205747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["login"], name: "index_users_on_login", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
