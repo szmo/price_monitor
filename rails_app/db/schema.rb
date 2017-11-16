25875284467ca256a361f3b870d9f2bd53bde627
Patryk Gałczyński <galczynski.patryk@gmail.com>
2017-11-16 19:53:23 +0100

    Feature/basic parser (#17)
    nokogiri setup, deal model, basic parser itaka

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115221927) do
+ActiveRecord::Schema.define(version: 20171116152553) do
 
   create_table "deals", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20171115221927) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
+    t.datetime "scraped_at"
+    t.integer "scrape_interval"
   end
 
   create_table "users", force: :cascade do |t|
