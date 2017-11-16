0d6848f34d04393a3bb42e47ea099dbf6e78b4d8
szmo <szymonduda10@gmail.com>
2017-11-16 20:57:52 +0100

    syntax fixes

--- rails_app/app/parsers/basic_parser.rb
+++ rails_app/app/parsers/basic_parser.rb
@@ -2,36 +2,36 @@ require 'nokogiri'
 require 'open-uri'
 
 class BasicParser
-    def initialize(url, name: nil)
-        @name = name.nil? ? URI(url).host : name
-        @url = url
-        @html = Nokogiri::HTML(open(url))
-    end
+  def initialize(url, name: nil)
+    @name = name.nil? ? URI(url).host : name
+    @url = url
+    @html = Nokogiri::HTML(open(url))
+  end
 
-    def find_in_html(xpath)
-        @html.css(xpath)[0].text
-    end
+  def find_in_html(xpath)
+    @html.css(xpath)[0].text
+  end
 
-    def purify_price(text)
-        # FIXME - period and comma for other currencies
-        text = text.strip
-        Settings.parser_helpers.things_to_delete.map { |thing| text = text.gsub(Regexp.new(thing), "") }
-        text.gsub(",", ".")
-    end
+  def purify_price(text)
+    # FIXME: - period and comma for other currencies
+    text = text.strip
+    Settings.parser_helpers.things_to_delete.map { |thing| text = text.gsub(Regexp.new(thing), '') }
+    text.tr(',', '.')
+  end
 
-    def purify_text(text)
-        text.strip
-    end
+  def purify_text(text)
+    text.strip
+  end
 
-    def parse
-        result = {:url => @url, :provider => @name}
-        Settings.parsers[@name].number.each do |property, xpath|
-            result[property] = purify_price(find_in_html(xpath))
-        end
-        Settings.parsers[@name].string.each do |property, xpath|
-            result[property] = purify_text(find_in_html(xpath))
-        end
-        Rails.logger.debug result
-        Deal.new(**result)
+  def parse
+    result = { url: @url, provider: @name }
+    Settings.parsers[@name].number.each do |property, xpath|
+      result[property] = purify_price(find_in_html(xpath))
     end
+    Settings.parsers[@name].string.each do |property, xpath|
+      result[property] = purify_text(find_in_html(xpath))
+    end
+    Rails.logger.debug result
+    Deal.new(**result)
+  end
 end
