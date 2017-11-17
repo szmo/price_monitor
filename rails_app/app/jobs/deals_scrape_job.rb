e9927d7e5f1e967f9f45ae87da80746e4f983534
Patryk Gałczyński <galczynski.patryk@gmail.com>
2017-11-17 08:40:37 +0100

    hot fix

--- rails_app/app/jobs/deals_scrape_job.rb
+++ rails_app/app/jobs/deals_scrape_job.rb
@@ -6,22 +6,30 @@ class DealsScrapeJob < ApplicationJob
   def perform(*args)
     # Do something later
     start_time = Time.now
+    processed_deals = 0
+    errors = 0
     deals_to_parse.each do |deal|
       # new_deal_status = Settings.parser_helpers.join_parsers[deal.provider].constantize.new(deal.url)
-      if deal.updated_at.nil? or deal.updated_at + deal.scrape_interval < Time.now
-        new_deal = BasicParser.new(deal.url).parse
-        deal_identifier = Digest::MD5.hexdigest(deal.url)
-        @@graphite.send_raw({
-          "deals.#{deal_identifier}.price" => new_deal.price,
-          "deals.#{deal_identifier}.time_since_update" => (Time.now - deal.updated_at),
-          "deals.#{deal_identifier}.smart_interval" => deal.scrape_interval,
-        })
-        deal.update!(price: new_deal.price, updated_at: Time.now)
+      begin
+        if deal.updated_at.nil? or deal.updated_at + deal.scrape_interval < Time.now
+          new_deal = BasicParser.new(deal.url).parse
+          deal_identifier = Digest::MD5.hexdigest(deal.url)
+          @@graphite.send_raw({
+            "deals.#{deal_identifier}.price" => new_deal.price,
+            "deals.#{deal_identifier}.time_since_update" => (Time.now - deal.updated_at),
+            "deals.#{deal_identifier}.smart_interval" => deal.scrape_interval,
+          })
+          deal.update!(price: new_deal.price, updated_at: Time.now)
+          processed_deals = processed_deals + 1
+        end
+      rescue
+        errors = errors + 1
       end
     end
     @@graphite.send_raw({
       "deal_worker.work_time" => (Time.now - start_time),
-      "deal_worker.number_of_deals_processed" => deals_to_parse.length,
+      "deal_worker.number_of_deals_processed" => processed_deals,
+      "deal_worker.number_of_errors" => errors,
       "deal_worker.number_of_active_workers" => 1
     })
     DealsScrapeJob.set(wait: 1.minute).perform_later
