b9034ac4bb5c7f5d65768a25afb396f77311b49f
szmo <szymonduda10@gmail.com>
2017-11-17 13:11:39 +0100

    fix

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create!([
-  {email: "szmo@ap.pl", encrypted_password: "$2a$11$/yImgrYqLoGDG7pst28Ze.zWxw4eyCgoehkMJf.NHsTMT.09NhjjG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-11-17 07:32:30", last_sign_in_at: "2017-11-17 07:32:30", current_sign_in_ip: "149.156.193.226", last_sign_in_ip: "149.156.193.226"}
-])
-Deal.create!([
-  {name: "Hotel Blue Reef Resort", price: 1539, country: "Egipt", city: "Marsa Alam", url: "https://www.itaka.pl/wczasy/egipt/marsa-alam/hotel-blue-reef-resort,RMFBLUE.html?ofr_id=8a2f9b27a50c19eb7b3768be7288b32b04203355a9dfe5b957de28d5ff223348&adults=2&childs=0&currency=PLN", scrape_interval: 60, disabled_at: nil, provider: "www.itaka.pl"},
-  {name: "Hotel Oasis Dunas", price: 1859, country: "Wyspy Kanaryjskie", city: "Fuerteventura", url: "https://www.itaka.pl/wczasy/wyspy-kanaryjskie/fuerteventura/hotel-oasis-dunas,FUEDUNA.html?ofr_id=ba359f7d106d05322309adbcbf90fc5b3935f67bb85ae582eaacb964049edd30&adults=2&childs=0&currency=PLN", scrape_interval: 600, disabled_at: nil, provider: "www.itaka.pl"},
-  {name: "Hotel Gala Tenerife", price: 3219, country: "Wyspy Kanaryjskie", city: "Teneryfa", url: "https://www.itaka.pl/wczasy/wyspy-kanaryjskie/teneryfa/hotel-gala-tenerife,TFSGALA.html?ofr_id=f5ae2486cdadb10e3b8ba3bc51d4fdfce4ccd6bfa81183426bce63f5b1b0a272&adults=2&childs=0&currency=PLN", scrape_interval: 3600, disabled_at: nil, provider: "www.itaka.pl"},
-  {name: "Samsung Galaxy S7 G930F 32GB czarny", price: 1998, country: nil, city: nil, url: "https://www.x-kom.pl/p/288297-smartfon-telefon-samsung-galaxy-s7-g930f-32gb-czarny.html", scrape_interval: 600, disabled_at: nil, provider: "www.x-kom.pl"},
-  {name: "Apple iPhone X 64GB Silver", price: 4979, country: nil, city: nil, url: "https://www.x-kom.pl/p/382264-smartfon-telefon-apple-iphone-x-64gb-silver.html", scrape_interval: 3600, disabled_at: nil, provider: "www.x-kom.pl"},
-  {name: "Hotel Águahotels Sal Vila Verde/Oasis Atlantico Belorizonte", price: 2449, country: "Wyspy Zielonego Przylądka", city: "", url: "https://www.itaka.pl/wczasy/wyspy-zielonego-przyladka/hotel-aguahotels-sal-vila-verdeoasis-atlantico-belorizonte,SIDVILB.html?ofr_id=6760a7ee9ab6c63e4cede42f33c8746d6a98b0828ef1cb357a00e497818a27fc&adults=2&childs=0&currency=PLN", scrape_interval: 60, disabled_at: nil, provider: "www.itaka.pl"}
-])
+# User.create!([
+#   {email: "szmo@ap.pl", encrypted_password: "$2a$11$/yImgrYqLoGDG7pst28Ze.zWxw4eyCgoehkMJf.NHsTMT.09NhjjG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-11-17 07:32:30", last_sign_in_at: "2017-11-17 07:32:30", current_sign_in_ip: "149.156.193.226", last_sign_in_ip: "149.156.193.226"}
+# ])
+# Deal.create!([
+#   {name: "Hotel Blue Reef Resort", price: 1539, country: "Egipt", city: "Marsa Alam", url: "https://www.itaka.pl/wczasy/egipt/marsa-alam/hotel-blue-reef-resort,RMFBLUE.html?ofr_id=8a2f9b27a50c19eb7b3768be7288b32b04203355a9dfe5b957de28d5ff223348&adults=2&childs=0&currency=PLN", scrape_interval: 60, disabled_at: nil, provider: "www.itaka.pl"},
+#   {name: "Hotel Oasis Dunas", price: 1859, country: "Wyspy Kanaryjskie", city: "Fuerteventura", url: "https://www.itaka.pl/wczasy/wyspy-kanaryjskie/fuerteventura/hotel-oasis-dunas,FUEDUNA.html?ofr_id=ba359f7d106d05322309adbcbf90fc5b3935f67bb85ae582eaacb964049edd30&adults=2&childs=0&currency=PLN", scrape_interval: 600, disabled_at: nil, provider: "www.itaka.pl"},
+#   {name: "Hotel Gala Tenerife", price: 3219, country: "Wyspy Kanaryjskie", city: "Teneryfa", url: "https://www.itaka.pl/wczasy/wyspy-kanaryjskie/teneryfa/hotel-gala-tenerife,TFSGALA.html?ofr_id=f5ae2486cdadb10e3b8ba3bc51d4fdfce4ccd6bfa81183426bce63f5b1b0a272&adults=2&childs=0&currency=PLN", scrape_interval: 3600, disabled_at: nil, provider: "www.itaka.pl"},
+#   {name: "Samsung Galaxy S7 G930F 32GB czarny", price: 1998, country: nil, city: nil, url: "https://www.x-kom.pl/p/288297-smartfon-telefon-samsung-galaxy-s7-g930f-32gb-czarny.html", scrape_interval: 600, disabled_at: nil, provider: "www.x-kom.pl"},
+#   {name: "Apple iPhone X 64GB Silver", price: 4979, country: nil, city: nil, url: "https://www.x-kom.pl/p/382264-smartfon-telefon-apple-iphone-x-64gb-silver.html", scrape_interval: 3600, disabled_at: nil, provider: "www.x-kom.pl"},
+#   {name: "Hotel Águahotels Sal Vila Verde/Oasis Atlantico Belorizonte", price: 2449, country: "Wyspy Zielonego Przylądka", city: "", url: "https://www.itaka.pl/wczasy/wyspy-zielonego-przyladka/hotel-aguahotels-sal-vila-verdeoasis-atlantico-belorizonte,SIDVILB.html?ofr_id=6760a7ee9ab6c63e4cede42f33c8746d6a98b0828ef1cb357a00e497818a27fc&adults=2&childs=0&currency=PLN", scrape_interval: 60, disabled_at: nil, provider: "www.itaka.pl"}
+# ])
