64bb6539d98f93fbe99a934496f3821aa4401ae7
szmo <szymonduda10@gmail.com>
2017-11-21 09:07:38 +0100

    scrape job refactor

--- rails_app/app/jobs/deals_scrape_job.rb
+++ rails_app/app/jobs/deals_scrape_job.rb
@@ -9,35 +9,40 @@ class DealsScrapeJob < ApplicationJob
     processed_deals = 0
     errors = 0
     deals_to_parse.each do |deal|
-      # new_deal_status = Settings.parser_helpers.join_parsers[deal.provider].constantize.new(deal.url)
       begin
-        if deal.updated_at.nil? or deal.updated_at + deal.scrape_interval < Time.now
-          new_deal = BasicParser.new(deal.url).parse
-          deal_identifier = Digest::MD5.hexdigest(deal.url)
-          @@graphite.send_raw({
-            "deals.#{deal_identifier}.price" => new_deal.price,
-            "deals.#{deal_identifier}.time_since_update" => (Time.now - deal.updated_at),
-            "deals.#{deal_identifier}.smart_interval" => deal.scrape_interval,
-          })
-          deal.update!(price: new_deal.price, updated_at: Time.now)
-          processed_deals = processed_deals + 1
-        end
+        new_deal = BasicParser.new(deal.url).parse
+        deal_identifier = deal.name.parameterize
+        send_deal_to_graphite(new_deal.price, Time.now - deal.updated_at, deal.scrape_interval, deal_identifier)
+        deal.update!(price: new_deal.price, updated_at: Time.now)
+        processed_deals += 1
       rescue
-        errors = errors + 1
+        errors += 1
       end
     end
-    @@graphite.send_raw({
-      "deal_worker.work_time" => (Time.now - start_time),
-      "deal_worker.number_of_deals_processed" => processed_deals,
-      "deal_worker.number_of_errors" => errors,
-      "deal_worker.number_of_active_workers" => 1
-    })
+    send_worker_stats_to_graphite(Time.now - start_time, processed_deals, errors)
     DealsScrapeJob.set(wait: 1.minute).perform_later
   end
 
   private
 
   def deals_to_parse
-    Deal.where(disabled_at: nil)
+    Deal.where(disabled_at: nil).where('updated_at + scrape_interval < ?', Time.now)
+  end
+
+  def send_deal_to_graphite(deal_price, time_since_update, deal_scrape_interval, deal_identifier)
+    @@graphite.send_raw({
+      "deals.#{deal_identifier}.price" => deal_price,
+      "deals.#{deal_identifier}.time_since_update" => time_since_update,
+      "deals.#{deal_identifier}.smart_interval" => deal_scrape_interval,
+    })
+  end
+
+  def send_worker_stats_to_graphite(worked_time, processed_deals, errors)
+    @@graphite.send_raw({
+      "deal_worker.work_time" => worked_time,
+      "deal_worker.number_of_deals_processed" => processed_deals,
+      "deal_worker.number_of_errors" => errors,
+      "deal_worker.number_of_active_workers" => 1
+    })
   end
 end
